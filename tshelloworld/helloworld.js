11c2296096d7103ed44b0f1ef13aca71d7b29fb7
Ramona Saintandre <codinggrandma@gmail.com>
2019-12-30 22:31:14 -0500

    created first TS error

--- tshelloworld/helloworld.js
+++ tshelloworld/helloworld.js
@@ -2,3 +2,4 @@ console.log('hello world');
 function hello(string) {
     console.log('hello' + string);
 }
+hello(1);
--- tshelloworld/helloworld.ts
+++ tshelloworld/helloworld.ts
@@ -3,3 +3,4 @@ console.log('hello world');
 function hello(string : String){
     console.log('hello' + string);
 }
+hello(1);
