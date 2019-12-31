163b48e0ad5f5ea99c3f5db1dd57505784321ee3
Ramona Saintandre <codinggrandma@gmail.com>
2019-12-30 23:06:25 -0500

    made comments on errors that I am recieving

--- tshelloworld/types.js
+++ tshelloworld/types.js
@@ -1,2 +1,20 @@
+// you should not use the any type if possible 
 var n = 1;
 n = 'robb';
+var isWinter = false;
+isWinter = 132;
+var count = 5;
+// not sure why it is telling me that name has already been delclared 
+var name = 'Bran';
+var names = ['john,shae,michael'];
+var Starks;
+(function (Starks) {
+    Starks[Starks["john"] = 0] = "john";
+    Starks[Starks["shae"] = 1] = "shae";
+    Starks[Starks["michael"] = 2] = "michael";
+})(Starks || (Starks = {}));
+;
+var cat = Starks.shae;
+function getName() {
+    return 'winter is coming';
+}
--- tshelloworld/types.ts
+++ tshelloworld/types.ts
@@ -1,3 +1,25 @@
 // you should not use the any type if possible 
 var n : any = 1;
 n = 'robb';
+var isWinter : boolean = false;
+isWinter = true;
+
+var count :Number =5;
+// not sure why it is telling me that name has already been delclared 
+// var name : String = 'Bran';
+
+
+var names : string[] = ['john,shae,michael'];
+
+enum Starks {john, shae ,michael};
+
+
+var cat : Starks = Starks.shae;
+
+// getting this error message Duplicate function implementation.ts(2393)
+// function getName() : string{
+//     return 'winter is coming';
+
+// }
+
+
